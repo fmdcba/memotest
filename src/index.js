52d33959d77036c90aca9282d0c54e64ab246960
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-03-26 23:45:31 -0300

    mejora interfaz centrando elemento en su cuadro

--- src/index.js
+++ src/index.js
@@ -1,156 +1,172 @@
-const $botonJugar = document.querySelector('button');
-const EMOJIS = ['😀', '😠', '😉', '😎', '😢', '😛', '😲', '🙄'];
-const emojisDuplicados = EMOJIS.concat(EMOJIS);
+const $botonJugar = document.querySelector("button");
+const EMOJIS = ["😀", "😠", "😉", "😎", "😢", "😛", "😲", "🙄"];
+const elementosTablero = EMOJIS.concat(EMOJIS);
 let eleccionUsuario = [];
 let eleccionesCorrectas = [];
 let intentos = 0;
 
-$botonJugar.onclick = function () {
-  const emojisDesordenados = desordenarEmojis(emojisDuplicados);
+$botonJugar.onclick = configurarTablero;
 
-  reiniciar();
-  actualizarTablero(emojisDesordenados);
-  animacionTablero();
-  setTimeout(manejarInput, 1000);
+function configurarTablero() {
+  const $tablero = document.querySelector("#tablero");
+  const $cuadros = document.querySelectorAll(".cuadro");
+  const elementosMezclados = mezclarElementos();
+
+  $cuadros.forEach((cuadro, indice) => {
+    cuadro.textContent = elementosMezclados[indice];
+  });
+
+  manejarInput($tablero);
 }
 
-function reiniciar() {
-  document.querySelectorAll('div').forEach(function (cuadro) {
-    cuadro.style.fontSize = 0;
-    habilitarCuadro(cuadro);
-    cuadro.classList.remove('text-bg-secondary');
-  })
-  document.querySelector('#mensaje').textContent = 'Encontrá los pares de emojis para ganar';
-  $botonJugar.textContent = 'Jugar';
-  eleccionUsuario = [];
-  eleccionesCorrectas = [];
-  intentos = 0;
+function mezclarElementos() {
+  return elementosTablero.sort(() => 0.7 - Math.random());
 }
 
-function desordenarEmojis(emojis) {
-  let m = emojis.length
-  let t;
-  let i;
-
-  while (m) {
-    i = Math.floor(Math.random() * m--);
-
-    t = emojis[m];
-    emojis[m] = emojis[i];
-    emojis[i] = t;
-  }
-
-  return emojis;
+function manejarInput($tablero) {
+  $tablero.onclick = (e) => {
+    console.log(e.target);
+  };
 }
 
-function actualizarTablero(emojis) {
-  document.querySelectorAll('.cuadro').forEach(function (cuadro, indice) {
-    cuadro.textContent = emojis[indice]
-    cuadro.id = indice;
-  })
-}
+// function reiniciar() {
+//   document.querySelectorAll("div").forEach(function (cuadro) {
+//     cuadro.style.fontSize = 0;
+//     habilitarCuadro(cuadro);
+//     cuadro.classList.remove("text-bg-secondary");
+//   });
+//   document.querySelector("#mensaje").textContent =
+//     "Encontrá los pares de emojis para ganar";
+//   $botonJugar.textContent = "Jugar";
+//   eleccionUsuario = [];
+//   eleccionesCorrectas = [];
+//   intentos = 0;
+// }
 
-function manejarInput() {
-  eleccionUsuario = [];
+// function desordenarEmojis(emojis) {
+//   let m = emojis.length;
+//   let t;
+//   let i;
 
-  document.querySelectorAll('.cuadro').forEach(function (cuadro) {
-    cuadro.onclick = validarEleccionUsuario;
-  })
-}
+//   while (m) {
+//     i = Math.floor(Math.random() * m--);
 
-function validarEleccionUsuario(event) {
-  const emoji = event.target;
-  eleccionUsuario.push(emoji);
+//     t = emojis[m];
+//     emojis[m] = emojis[i];
+//     emojis[i] = t;
+//   }
 
-  if (emoji.id !== eleccionUsuario[0].id || eleccionUsuario.length === 1) {
-    if (eleccionUsuario.length === 2) {
-      intentos++;
-      bloquearInputUsuario();
-    }
+//   return emojis;
+// }
 
-    resaltarEmoji(emoji);
+// function actualizarTablero(emojis) {
+//   document.querySelectorAll(".cuadro").forEach(function (cuadro, indice) {
+//     cuadro.textContent = emojis[indice];
+//     cuadro.id = indice;
+//   });
+// }
 
-    setTimeout(function() {
-      if (eleccionUsuario.length === 2) {
-        const sonIguales = compararElementos();
+// function manejarInput() {
+//   eleccionUsuario = [];
 
-        if (sonIguales) {
-          eleccionesCorrectas.forEach(function(cuadro) {
-            deshabilitarCuadro(cuadro);
-          })
-          manejarInput();
-        } else {
-          eleccionUsuario.forEach(function(cuadro) {
-            habilitarCuadro(cuadro);
-          })
-          ocultarEmoji(emoji);
-          manejarInput();
-        }
-      }
+//   document.querySelectorAll(".cuadro").forEach(function (cuadro) {
+//     cuadro.onclick = validarEleccionUsuario;
+//   });
+// }
 
-      if(eleccionesCorrectas.length === emojisDuplicados.length) {
-        terminarPartida();
-      }
-    }, 2000);
-  } else {
-    eleccionUsuario.pop();
-  }
+// function validarEleccionUsuario(event) {
+//   const emoji = event.target;
+//   eleccionUsuario.push(emoji);
 
-}
+//   if (emoji.id !== eleccionUsuario[0].id || eleccionUsuario.length === 1) {
+//     if (eleccionUsuario.length === 2) {
+//       intentos++;
+//       bloquearInputUsuario();
+//     }
 
-function compararElementos() {
-  const primerElemento = eleccionUsuario[0];
-  const segundoElemento = eleccionUsuario[eleccionUsuario.length -1];
+//     resaltarEmoji(emoji);
 
-  if (primerElemento.textContent === segundoElemento.textContent) {
-    eleccionesCorrectas.push(primerElemento);
-    eleccionesCorrectas.push(segundoElemento);
-    return true;
-  } else {
-    ocultarEmoji(primerElemento);
-    ocultarEmoji(segundoElemento)
-    return false;
-  }
-}
+//     setTimeout(function () {
+//       if (eleccionUsuario.length === 2) {
+//         const sonIguales = compararElementos();
 
-function resaltarEmoji(emoji) {
-  emoji.style.fontSize = 'xx-large';
-  emoji.classList.add('text-bg-secondary');
-}
+//         if (sonIguales) {
+//           eleccionesCorrectas.forEach(function (cuadro) {
+//             deshabilitarCuadro(cuadro);
+//           });
+//           manejarInput();
+//         } else {
+//           eleccionUsuario.forEach(function (cuadro) {
+//             habilitarCuadro(cuadro);
+//           });
+//           ocultarEmoji(emoji);
+//           manejarInput();
+//         }
+//       }
 
+//       if (eleccionesCorrectas.length === emojisDuplicados.length) {
+//         terminarPartida();
+//       }
+//     }, 2000);
+//   } else {
+//     eleccionUsuario.pop();
+//   }
+// }
 
-function ocultarEmoji(emoji) {
-  emoji.style.fontSize = 0;
-  emoji.classList.remove('text-bg-secondary');
-}
+// function compararElementos() {
+//   const primerElemento = eleccionUsuario[0];
+//   const segundoElemento = eleccionUsuario[eleccionUsuario.length - 1];
 
-function terminarPartida() {
-  document.querySelector('#mensaje').textContent = `Ganaste y te tomó ${intentos} intentos`;
-  $botonJugar.textContent = 'Reiniciar';
-}
+//   if (primerElemento.textContent === segundoElemento.textContent) {
+//     eleccionesCorrectas.push(primerElemento);
+//     eleccionesCorrectas.push(segundoElemento);
+//     return true;
+//   } else {
+//     ocultarEmoji(primerElemento);
+//     ocultarEmoji(segundoElemento);
+//     return false;
+//   }
+// }
 
-function animacionTablero() {
-  document.querySelectorAll('.cuadro').forEach(function (cuadro) {
-    cuadro.classList.add('text-bg-secondary');
+// function resaltarEmoji(emoji) {
+//   emoji.style.fontSize = "xx-large";
+//   emoji.classList.add("text-bg-secondary");
+// }
 
-    setTimeout(function () {
-      cuadro.classList.remove('text-bg-secondary');
-    }, 500);
-  })
-}
+// function ocultarEmoji(emoji) {
+//   emoji.style.fontSize = 0;
+//   emoji.classList.remove("text-bg-secondary");
+// }
 
-function bloquearInputUsuario() {
-  document.querySelectorAll('.cuadro').forEach(function (cuadro) {
-    cuadro.onclick = function() {};
-  })
-}
+// function terminarPartida() {
+//   document.querySelector(
+//     "#mensaje"
+//   ).textContent = `Ganaste y te tomó ${intentos} intentos`;
+//   $botonJugar.textContent = "Reiniciar";
+// }
 
-function deshabilitarCuadro(cuadro) {
-  if (eleccionUsuario.length !== 2 || eleccionesCorrectas.length >= 2) {
-    cuadro.classList.remove('cuadro');
-  }
-}
+// function animacionTablero() {
+//   document.querySelectorAll(".cuadro").forEach(function (cuadro) {
+//     cuadro.classList.add("text-bg-secondary");
 
-function habilitarCuadro(cuadro) {
-  cuadro.classList.add('cuadro');
-}
+//     setTimeout(function () {
+//       cuadro.classList.remove("text-bg-secondary");
+//     }, 500);
+//   });
+// }
+
+// function bloquearInputUsuario() {
+//   document.querySelectorAll(".cuadro").forEach(function (cuadro) {
+//     cuadro.onclick = function () {};
+//   });
+// }
+
+// function deshabilitarCuadro(cuadro) {
+//   if (eleccionUsuario.length !== 2 || eleccionesCorrectas.length >= 2) {
+//     cuadro.classList.remove("cuadro");
+//   }
+// }
+
+// function habilitarCuadro(cuadro) {
+//   cuadro.classList.add("cuadro");
+// }
