73f4ea3a280a21d6dacfffd0a9203330f2c8252d
Facundo Manuel Díaz Córdoba <facudiaz1738@hotmail.com>
2024-03-29 18:07:58 -0300

    prueba que el tablero se muestre y los cuadros sean aleatorios

--- cypress/e2e/memotest.cy.js
+++ cypress/e2e/memotest.cy.js
@@ -1,9 +1,38 @@
 const URL = "http://127.0.0.1:8080/";
+const NUMERO_CUADROS = 16;
 
-describe("test de integracion memotest", () => {
-  beforeEach(() => {
-    cy.visit(URL);
+context("test de integracion memotest", () => {
+  describe("juega memotest", () => {
+    beforeEach(() => {
+      cy.visit(URL);
+    });
+
+    it("se asegura que se muestre un tablero con cuadros", () => {
+      cy.get(".cuadro").should("have.lengthOf", NUMERO_CUADROS);
+    });
+
+    it("se asegura que al jugar los cuadros sean aleatorios", () => {
+      cy.get(".btn").click();
+
+      cy.get(".cuadro").then((cuadros) => {
+        let cuadrosOriginales = [];
+        cuadros.each(function (i, cuadroOriginal) {
+          cuadrosOriginales.push(cuadroOriginal.textContent);
+        });
+
+        cy.get(".btn").click();
+
+        cy.get(".cuadro").then((nuevosCuadros) => {
+          let cuadrosNuevos = [];
+          nuevosCuadros.each(function (i, cuadroNuevo) {
+            cuadrosNuevos.push(cuadroNuevo);
+          });
+
+          cy.wrap(cuadrosOriginales).should("not.deep.equal", cuadrosNuevos);
+        });
+      });
+    });
+
+    it("elige una combinación incorrecta de cuadros", () => {});
   });
-
-  it("se asegura que se muestre un tablero con cuadros", () => {});
 });
